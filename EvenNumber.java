090dd6a51763133c7a5e693092b2ac991f528ce1
Kalyani <144818402+kalyanisalalkar@users.noreply.github.com>
2023-12-25 11:58:56 +0530

    Even Number program

---
+++ EvenNumber.java
@@ -0,0 +1,13 @@
+public class EvenNumber {
+
+	public static void main(String[] args) {
+		int  n = 3;
+		
+		
+		String result = n%2==0 ? "even" : "odd";
+		System.out.println ("The number " + n + " is : " + result);
+		
+
+	}
+
+}
